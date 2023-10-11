ed7bf30a1269933b1ed9b1e0ee58cf188e21ba83
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-10-11 16:48:29 +1300

    Initial commit

---
+++ component.mk
@@ -0,0 +1,9 @@
+#
+# "main" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+ifdef CONFIG_AUDIO_BOARD_CUSTOM
+COMPONENT_ADD_INCLUDEDIRS += ./esphome_board
+COMPONENT_SRCDIRS += ./esphome_board
+endif
