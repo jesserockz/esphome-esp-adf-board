4b15e4aca2596ce0346e640d62f31b1ad8e20cb8
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-10-11 17:23:59 +1300

    sd pins

--- esphome_board/board_def.h
+++ esphome_board/board_def.h
@@ -3,6 +3,21 @@
 #define _AUDIO_BOARD_DEFINITION_H_
 
 #define FUNC_SDCARD_EN (0)
+#define SDCARD_INTR_GPIO -1
+#define SDCARD_PWR_CTRL -1
+#define ESP_SD_PIN_CLK -1
+#define ESP_SD_PIN_CMD -1
+#define ESP_SD_PIN_D0 -1
+#define ESP_SD_PIN_D1 -1
+#define ESP_SD_PIN_D2 -1
+#define ESP_SD_PIN_D3 -1
+#define ESP_SD_PIN_D4 -1
+#define ESP_SD_PIN_D5 -1
+#define ESP_SD_PIN_D6 -1
+#define ESP_SD_PIN_D7 -1
+#define ESP_SD_PIN_CD -1
+#define ESP_SD_PIN_WP -1
+
 #define FUNC_SYS_LEN_EN (0)
 #define FUNC_SYS_LEN_EN (0)
 #define FUNC_LCD_SCREEN_EN (0)
