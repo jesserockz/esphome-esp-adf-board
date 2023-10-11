956fa0941cdf5a73c0f1bdfba8ad074f8afc26ee
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-10-11 17:20:10 +1300

    Disable stuff

--- esphome_board/board_def.h
+++ esphome_board/board_def.h
@@ -2,6 +2,12 @@
 #ifndef _AUDIO_BOARD_DEFINITION_H_
 #define _AUDIO_BOARD_DEFINITION_H_
 
+#define FUNC_SDCARD_EN (0)
+#define FUNC_SYS_LEN_EN (0)
+#define FUNC_SYS_LEN_EN (0)
+#define FUNC_LCD_SCREEN_EN (0)
+#define FUNC_LCD_TOUCH_EN (0)
+
 #define BOARD_PA_GAIN (0)
 
 #endif
